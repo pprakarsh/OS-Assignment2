055ddd40586670e54e7b5e1d92c8d262e8fa3049
prakarsh <prakarshparashar@gmail.com>
2019-10-11 13:01:23 +0530

    Prob1 completed, working

--- Problem1.c
+++ Problem1.c
@@ -11,7 +11,7 @@ struct queue
 	int size;
 	int front;
 	int rear;
-	int* q;
+	int q[1000];
 };
 
 struct shm_count
@@ -20,12 +20,13 @@ struct shm_count
 	int nodes_at_level;
 	int arr[10][1000];
 	struct queue queue;
+	int flag;
 };
 
 void make_queue(int cap, struct shm_count* ptr_node)
 {
-	ptr_node->queue.q = (int* )malloc(sizeof(int)*cap);	
 	ptr_node->queue.capacity = cap;
+	printf("Hello\n");
 	ptr_node->queue.front = 0;
 	ptr_node->queue.rear = -1;
 	ptr_node->queue.size = 0;
@@ -68,8 +69,6 @@ void push(struct shm_count* ptr_node, int val)
 
 int main(int argc, char* argv[])
 {
-	printf("\nHello, Chai Pilo");
-	
 	int v1 = atoi(argv[1]);
 	int v2 = atoi(argv[2]);
 	if(argc != 3)
@@ -78,12 +77,11 @@ int main(int argc, char* argv[])
 		exit(0);
 	}
 	
-	key_t key = ftok("~/OS_Assignment2/shm_count.txt", 'k');
+	key_t key = ftok("~/OS_Assignment2/shm_count_new.txt", 's');
 	int shm_id = shmget(key, sizeof(struct shm_count), 0666|IPC_CREAT);
 	struct shm_count* ptr_node = shmat(shm_id, NULL, 0);
 
 	make_queue(1000, ptr_node);
-	printf("Hello %d\n",getpid());
 
 	ptr_node->arr[0][0] = getpid();
 
@@ -91,79 +89,15 @@ int main(int argc, char* argv[])
 	r = 0;
 	c = 0;
 	
-	for(i = 0; i < v1; i++)
-	{
-		pid_t p = fork();
-		
-		if(p < 0)
-		{
-			printf("Error! Forking not possible\n");
-			exit(0);
-		}	
-		else if(p == 0)
-		{
-			c = i;
-			r = 1;
-			ptr_node->arr[r][c] = getpid();
-			printf("\nptr_node->arr[r][c]: %d\n", ptr_node->arr[r][c]);
-			kill(getpid(), SIGSTOP);
-			printf("Hello I am child! row-%d :)%d\n", r, getpid());
-			if(r == v2)
-			{
-				exit(0);
-			}
-			break;
-		}
-		else
-		{
-			continue;
-		}
-	}
+	ptr_node->flag = 0;	
 	
-	if(i == v1)
-	{
-		//Parent Process- checks if all children have been stopped
-		for(int i = 0; i < v1; i++)
-		{
-			waitpid(ptr_node->arr[r+1][i], NULL, WUNTRACED);
-		}
-		for(int i = 0; i < v1; i++)
-		{
-			if(!isFull(ptr_node))
-			{
-				printf("\nPushing: %d", ptr_node->arr[r+1][i]);
-				push(ptr_node, ptr_node->arr[r+1][i]);
-			}
-			else
-			{
-				printf("\nError! Queue is Full");
-			}
-		}
-
-		int pid;
-		if(!isEmpty(ptr_node))
-		{
-			pid = pop(ptr_node);
-			printf("\npid print: %d, r-%d, c-%d\n", pid, r, c);
-		}
-		else
-		{
-			printf("\nError! Queue is empty");
-		}
-		kill(pid, SIGCONT);	
-
-		//Parent Process- waits for all children to finish before exiting
-		while(wait(NULL) != -1);
-	}
-
-
+	// sleep(5);
+	int count = 0;
 	while(1)
 	{
-		printf("Hello, While loop\n");
 		int j;
-		for(j = 0; j < c+1; j++)
+		for(j = 0; (j < c+1) || (count == 0 && j < v1); j++)
 		{
-			//printf("Hello %d\n",getpid());
 			pid_t p = fork();
 			
 			if(p < 0)
@@ -173,13 +107,12 @@ int main(int argc, char* argv[])
 			}	
 			else if(p == 0)
 			{
-				//printf("Hello. I am child! %d\n",getpid());
 				c = c*(c+1)/2 + j;
 				r = r+1;
 				ptr_node->arr[r][c] = getpid();
-				printf("\nptr_node->arr[r][c]: %d\n", ptr_node->arr[r][c]);
 				kill(getpid(), SIGSTOP);
 				printf("Hello I am child! row-%d :)%d\n", r, getpid());
+				sleep(5);
 				if(r == v2)
 				{
 					if(!isEmpty(ptr_node))
@@ -191,42 +124,36 @@ int main(int argc, char* argv[])
 					{
 						printf("\nError! Queue is empty");
 					}
+					sleep(45);
 					exit(0);
 				}
 				break;
-				
+					
 			}
 			else
 			{
-				continue;
-			}
-			
-		}
-		if(j == c+1)
-		{
-			//Parent Process- checks if all children have been stopped
-			for(int i = c*(c+1)/2; i < (c*(c+1)/2)+c+1; i++)
-			{
-				waitpid(ptr_node->arr[r+1][i], NULL, WUNTRACED);
-			}
-			for(int i = c*(c+1)/2; i < (c*(c+1)/2)+c+1; i++)
-			{
+				waitpid(p, NULL, WUNTRACED);
+
 				if(!isFull(ptr_node))
 				{
-					printf("\nPushing: %d", ptr_node->arr[r+1][i]);
-					push(ptr_node, ptr_node->arr[r+1][i]);
+					push(ptr_node, p);
 				}
 				else
 				{
 					printf("\nError! Queue is Full");
 				}
-			}
 
+				continue;
+			}
+			
+		}
+
+		if(j == c+1 || (count == 0 && j == v1))
+		{
 			int pid;
 			if(!isEmpty(ptr_node))
 			{
 				pid = pop(ptr_node);
-				printf("\npid print: %d, r-%d, c-%d\n", pid, r, c);
 			}
 			else
 			{
@@ -236,18 +163,37 @@ int main(int argc, char* argv[])
 			
 			//Parent: will wait for its children to terminate
 			while(wait(NULL) != -1);
-			exit(0);
+			if(count != 0)
+			{
+				exit(0);
+			}
+			else
+			{
+				break;
+			}
 		}
+		count++;
 	}
-	shmdt(ptr_node);
-	shmctl(shm_id,IPC_RMID,NULL);
-	for(int l = 0; l < v2; l++)
+
+	int n_nodes[v2+1];
+	int prev = v1;
+	n_nodes[0] = 1;
+	for(int i = 1; i <= v2; i++)
+	{
+		n_nodes[i] = prev;
+		prev = prev*(prev+1)/2;
+	}
+	for(int l = 0; l <= v2; l++)
 	{
 		printf("\nLevel %d:  ", l);
-		for(int j = 0; j < sizeof(ptr_node->arr[l])/sizeof(int); j++)
+		for(int j = 0; j < n_nodes[l]; j++)
 		{
 			printf("%d ", ptr_node->arr[l][j]);
 		}
 	}
+	printf("\n");
+
+	shmdt(ptr_node);
+	shmctl(shm_id,IPC_RMID,NULL);
 	exit(0);
 }
